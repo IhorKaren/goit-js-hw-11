498c71b5c7682437605297a8794c67594ae9ef80
Ihor Karen <karenihor@gmail.com>
2023-03-08 01:16:47 +0200

    smooth scroll

--- src/index.js
+++ src/index.js
@@ -41,6 +41,7 @@ async function handleLoadMoreBtnClick() {
   currentPage += 1;
   const response = await fetchImages(formValue);
   makeGalleryUI(response);
+  smoothScroll()
 }
 
 async function fetchImages(searchValue) {
@@ -96,6 +97,18 @@ function makeGalleryUI(photos) {
   });
 }
 
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector(".gallery")
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+  });
+}
+
 function clearMarkup() {
   refs.galleryEl.innerHTML = '';
 }
+
