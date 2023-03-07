722bc47bce149bcc71e169651b0d9dd3a6471917
Ihor Karen <karenihor@gmail.com>
2023-03-07 20:47:30 +0200

    SimpleLightBox

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import makeGalleryUI from './js/make-markup'
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const API_KEY = '34207021-c15ce293e39f116dd33171e61';
 const URL = 'https://pixabay.com/api/';
@@ -9,6 +9,7 @@ const URL = 'https://pixabay.com/api/';
 const refs = {
   formEl: document.querySelector('.search-form'),
   buttonEl: document.querySelector('[type="submit"]'),
+  galleryEl: document.querySelector('.gallery'),
 };
 
 refs.formEl.addEventListener('submit', handleFormSubmit);
@@ -16,7 +17,7 @@ refs.formEl.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit(e) {
   e.preventDefault();
 
-  const formValue = e.target.elements.searchQuery.value.trim();
+  const formValue = e.target.elements.searchQuery.value;
 
   if (formValue === '') {
     Notiflix.Notify.info('Please fill in the input field for image search');
@@ -24,7 +25,7 @@ function handleFormSubmit(e) {
   }
 
   fetchImages(formValue).then(makeGalleryUI);
-  
+  refs.galleryEl.innerHTML = '';
 }
 
 async function fetchImages(searchValue) {
@@ -39,4 +40,46 @@ async function fetchImages(searchValue) {
   });
 
   return response.data.hits;
+}
+
+function makeGalleryUI(photos) {
+  const markup = photos
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+        <a class='gallery__link' href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${downloads}</b>
+          </p>
+        </div>
+      </div>`;
+      }
+    )
+    .join('');
+
+  refs.galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 300,
+  });
 }
