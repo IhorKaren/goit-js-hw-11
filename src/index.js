aff6329bc970ee2bace00ef052be7fc27a6a469b
Ihor Karen <karenihor@gmail.com>
2023-03-07 18:46:49 +0200

    fix build

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
+
+const refs = {
+    formEl: document.querySelector('.search-form'),
+    buttonEl: document.querySelector('[type="submit"]'),
+}
+
+refs.formEl.addEventListener('submit', handleFormSubmit)
+
+function handleFormSubmit(e) {
+    e.preventDefault();
+
+    const formValue = e.target.elements.searchQuery.value;
+}
